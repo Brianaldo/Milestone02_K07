479ca23054766f396a75141222dfb901fde44ccf
Thariq Zhafran Satyagraha <thoriqzs@Thariqs-MacBook-Air.local>
2021-08-02 23:05:48 +0700

    template dasar masih gambaran doang bisa diotak-atik lagi

---
+++ paice/src/header/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+// isi ini logonya sama navbar
+
+const Header = ()=>{
+    return(
+    <>
+        <header>
+            <h3>navbar logo</h3>
+        </header>
+    </>)
+}
+
+export default Header;
