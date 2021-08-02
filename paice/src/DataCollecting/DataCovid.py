d3b0be518f5c95ea4f0bae741337e4efafc26403
rayhankinan <rayhankinan@gmail.com>
2021-08-02 23:37:20 +0700

    Menambahkan sebagai fungsi

--- paice/src/DataCollecting/DataCovid.py
+++ paice/src/DataCollecting/DataCovid.py
@@ -4,12 +4,13 @@ import pandas as pd
 Data ini akan terus diupdate oleh Wikipedia setiap harinya
 '''
 
-Table = pd.read_html("https://en.wikipedia.org/wiki/Statistics_of_the_COVID-19_pandemic_in_Indonesia", match="COVID-19 cases in Indonesia")
+def UpdateData():
+    Table = pd.read_html("https://en.wikipedia.org/wiki/Statistics_of_the_COVID-19_pandemic_in_Indonesia", match="COVID-19 cases in Indonesia")
 
-df = Table[0]["COVID-19 cases in Indonesia[a][b]"]
+    df = Table[0]["COVID-19 cases in Indonesia[a][b]"]
 
-df = df.drop(df.index[35])
-df = df.drop(["Recoveryrate", "Fatalityrate", "Official website"], axis=1)
-df = df.rename(columns={"Active[c]" : "Active", "Cases per100,000 population[d]" : "Cases_per_100000_population"})
+    df = df.drop(df.index[35])
+    df = df.drop(["Recoveryrate", "Fatalityrate", "Official website"], axis=1)
+    df = df.rename(columns={"Active[c]" : "Active", "Cases per100,000 population[d]" : "Cases_per_100000_population"})
 
-df.to_csv("paice/src/DataCollecting/data_covid.csv", index=False)
+    df.to_csv("paice/src/DataCollecting/data_covid.csv", index=False)
