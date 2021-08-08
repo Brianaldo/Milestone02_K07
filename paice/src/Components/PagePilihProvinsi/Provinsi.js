ec80f3d46d6f240888816c51ae777735ed93796f
Brianaldo <brianaldophandiarta31@gmail.com>
2021-08-08 18:32:53 +0700

    edit page pilih prov

--- paice/src/Components/PagePilihProvinsi/Provinsi.js
+++ paice/src/Components/PagePilihProvinsi/Provinsi.js
@@ -1,4 +1,7 @@
 import "./Provinsi.css";
+import RumahSakit from "./RumahSakit.png";
+import InfoOksigen from "./InfoOksigen.png";
+import LokasiVaksinasi from "./LokasiVaksinasi.png";
 
 const Provinsi = (props) => {
   const clickHandler = () => {
@@ -6,7 +9,27 @@ const Provinsi = (props) => {
   };
   return (
     <button className="provinsi" type="button" onClick={clickHandler}>
-      {props.label}
+      <div className="provinsi__text">
+        <h2>{props.label}</h2>
+        <p>xx En</p>
+      </div>
+      <div className="provinsi__list-kategori">
+        {props.rs === true && (
+          <div className="provinsi__kategori">
+            <img src={RumahSakit} alt="" />
+          </div>
+        )}
+        {props.io === true && (
+          <div className="provinsi__kategori">
+            <img src={InfoOksigen} alt="" />
+          </div>
+        )}
+        {props.lv === true && (
+          <div className="provinsi__kategori">
+            <img src={LokasiVaksinasi} alt="" />
+          </div>
+        )}
+      </div>
     </button>
   );
 };
