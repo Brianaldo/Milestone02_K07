8d1661c4839e75fafabee62591b05b606cdf5fe4
Thariq Zhafran Satyagraha <thoriqzs@192.168.1.41>
2021-08-09 14:34:09 +0700

    benerin layout lagi

--- paice/src/Components/header/Nav.js
+++ paice/src/Components/header/Nav.js
@@ -1,16 +1,18 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { MenuFoldOutlined } from '@ant-design/icons';
+
 // isi ini logonya sama navbar
 
 const Nav = ()=>{
 
     return(
-        <>
+        <div className='container-header header'>
             <Link to='/'>
                 <button>Back</button>
             </Link>
-            <h3>navbar(kalo jadi)</h3>
-        </>
+            <MenuFoldOutlined/>
+        </div>
     )
 }
 
