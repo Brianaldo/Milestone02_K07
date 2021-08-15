eac2ce26985491e7012c6e95ba8304c3e259516c
thoriqzs25 <thoriqzahfran@yahoo.com>
2021-08-15 18:58:35 +0700

    nambahin telusuri di depan

--- src/src/Components/body/LandingPage.js
+++ src/src/Components/body/LandingPage.js
@@ -3,7 +3,10 @@ import React from 'react';
 const LandingPage = ()=>{
     return(
         <div className='landing-page'>
-            <img src={`${process.env.PUBLIC_URL}/home.png`}/>
+            <div className='button-landing'>
+                <button id='telusuri-button'>TELUSURI</button>
+                <button id='contact-button'>CONTACT US</button>
+            </div>
         </div>
     )
 };
